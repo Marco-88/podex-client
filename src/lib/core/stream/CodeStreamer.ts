30d9413a745fe17b7e5e967d1f342b83969f337a
Marco <s66853@beuth-hochschule.de>
2021-09-28 20:24:41 +0200

    Handle issue of received double chunks in stream from api

--- src/lib/core/stream/CodeStreamer.ts
+++ src/lib/core/stream/CodeStreamer.ts
@@ -4,6 +4,7 @@ import * as uuid from 'uuid';
 export class CodeStreamer {
     private store: ItemStore<StoreItem>;
     private readonly reader: ReadableStreamReader<Uint8Array>;
+    private tempToken: string;
 
     constructor(itemStore: ItemStore<StoreItem>, reader: ReadableStreamReader<Uint8Array>) {
         this.store = itemStore;
@@ -16,8 +17,17 @@ export class CodeStreamer {
         this.store.add(item);
 
         for await (const chunk of CodeStreamer.asyncIterableFromStream(this.reader)) {
-            const token = CodeStreamer.processCompletionToken(chunk);
-            this.store.updateItem(id, token);
+            this.storeToken(id, chunk);
+        }
+    }
+
+    private storeToken(id: string, chunk: Uint8Array) {
+        const token = this.processCompletionToken(chunk);
+        this.store.updateItem(id, token);
+
+        if(this.tempToken) {
+            this.store.updateItem(id, JSON.parse(this.tempToken));
+            this.tempToken = undefined;
         }
     }
 
@@ -29,11 +39,11 @@ export class CodeStreamer {
         }
     }
 
-    private static processCompletionToken(chunk: Uint8Array): string {
+    private processCompletionToken(chunk: Uint8Array): string {
         const token = CodeStreamer.byteToString(chunk);
 
-        if (token.startsWith('data:')) {
-            const item = CodeStreamer.parseCompletion(token);
+        if (token.startsWith('data: ')) {
+            const item = this.parseCompletion(token);
 
             if (item && item.choices.length > 0)
                 return item.choices[0].text
@@ -41,9 +51,25 @@ export class CodeStreamer {
         return '';
     }
 
-    private static parseCompletion(token: string): Completion {
-        token = token.substr(6).replace('data: [DONE]', '').trim();
+    private parseCompletion(token: string): Completion {
+        token = token.substr(6).replace('data: [DONE]', '');
+        token = this.ifDoubleDataSetTemp(token);
         return JSON.parse(token);
+    }
+
+    private ifDoubleDataSetTemp(token: string): string {
+        if (CodeStreamer.checkTokenForDoubleData(token)) {
+            const tokens = token.split('data: {"id": "cmpl-')
+            console.log("Found double data in token: : ", tokens[0]);
+            this.tempToken = '{"id": "cmpl-' + tokens[1];
+            return tokens[0];
+        }
+        return token;
+    }
+
+    private static checkTokenForDoubleData(token: string): boolean {
+        return token.includes('data: {"id": "cmpl-') &&
+            token.indexOf('data: {"id": "cmpl-') < token.lastIndexOf("davinci-codex:2021-08-03")
     }
 
     private static async* asyncIterableFromStream<T>(reader: ReadableStreamReader<T>): AsyncIterable<T> {
