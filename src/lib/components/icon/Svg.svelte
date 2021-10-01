293d4c82bf63bc145e9cc813e2afa462b21a60c2
Marco <s66853@beuth-hochschule.de>
2021-10-01 02:16:20 +0200

    Move icons to separate file

--- src/lib/components/icon/Svg.svelte
+++ src/lib/components/icon/Svg.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import ArrowRight from './svg/arrow-right.svg?component';
-	import Check from './svg/check.svg?component';
-	import Close from './svg/times.svg?component';
-	import Edit from './svg/pen.svg?component';
-	import Trash from './svg/trash-alt.svg?component';
-	import Toilet from './svg/toilet.svg?component';
+	import {icons} from './icons';
 
-	export let width = 32;
-	export let height = 32;
-	export let key: string;
-	let SvgIcon;
+	export let size = 32;
+	export let key = 'toilet';
 
-	onMount(() => {
-		if (key === 'arrowRight') SvgIcon = ArrowRight;
-		else if (key === 'check') SvgIcon = Check;
-		else if (key === 'close') SvgIcon = Close;
-		else if (key === 'edit') SvgIcon = Edit;
-		else if (key === 'trash') SvgIcon = Trash;
-		else SvgIcon = Toilet;
-	});
+	const {icon, top} = icons[key];
+	const topAttr = top ? ` top: ${top};` : '';
 </script>
 
-<svelte:component this={SvgIcon} {width} {height}/>
+<div class='svg-wrapper' style={`height: ${size}px; width: ${size}px;${topAttr}`}>
+	<svelte:component this={icon} />
+</div>
+
+<style>
+	.svg-wrapper {
+		position: absolute;
+	}
+</style>
---
+++ src/lib/components/icon/icons.ts
@@ -0,0 +1,15 @@
+import ArrowRight from './svg/arrow-right.svg?component';
+import Check from './svg/check.svg?component';
+import Close from './svg/times.svg?component';
+import Edit from './svg/pen.svg?component';
+import Toilet from './svg/toilet.svg?component';
+import Trash from './svg/trash-alt.svg?component';
+
+export const icons = {
+	submit: { top: '.45rem', icon: ArrowRight },
+	check: { top: '.55rem', icon: Check },
+	close: { top: '.4rem', icon: Close },
+	edit: { top: '.34rem', icon: Edit },
+	toilet: { icon: Toilet },
+	trash: { top: '.4rem', icon: Trash }
+}
