019fcfad90f4ebf47c3e70898fbd34057ab9bed8
Marco <s66853@beuth-hochschule.de>
2021-10-01 02:18:01 +0200

    Add module declaration to global type defs

--- global.d.ts
+++ global.d.ts
@@ -3,3 +3,13 @@ declare interface ImportMeta {
     VITE_OPENAI_API_KEY: string
   }
 }
+
+declare module '*.svg' {
+  const content: string
+  export default content
+}
+
+declare module '*.svg?component' {
+  const content: string
+  export default content
+}
