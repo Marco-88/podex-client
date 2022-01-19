a46de912308afe2d3018277d3a02a92690e6a812
Marco <s66853@beuth-hochschule.de>
2022-01-19 23:33:39 +0100

    Add more flexibility of select component to handle also loading of engines

--- src/lib/components/basic/Select.svelte
+++ src/lib/components/basic/Select.svelte
@@ -1,38 +1,59 @@
 <script lang="ts">
+	import Icon from '../icon/Icon.svelte';
 	export let text = '';
 	export let value = 'plaintext';
 	export let saveValue: (value: string) => void;
 	export let items = [];
+	export let capitalized = true;
+	export let loading = false;
 
 	const capitalize = (word: string): string => word.charAt(0).toUpperCase() + word.slice(1);
 	const getId = (): string => text.toLowerCase().replace(/ /g, '-')
 	const setValue = (): void => saveValue(selected.toLowerCase());
-
-	$: selected = capitalize(value)
+	$: selected = capitalized ? capitalize(value) : value;
 </script>
 
-<div class='select-wrapper'>
+<div class='select-wrapper' class:loading>
 	<label for={getId()}> {text}: </label>
-	<select id={getId()} bind:value={selected} on:change={() => setValue()} >
-		{#each items as item}
-			<option class='option'>{capitalize(item)}</option>
-		{/each}
-	</select>
+
+	{#if loading}
+		<div class='spinner'>
+			<Icon key="spinner" fill={true} size={24} tooltip='Loading Engines'/>
+		</div>
+	{:else}
+		<select id={getId()} bind:value={selected} on:change={() => setValue()} >
+			{#each items as item}
+				<option class='option'>{capitalized ? capitalize(item) : item}</option>
+			{/each}
+		</select>
+	{/if}
 </div>
 
 <style lang='scss'>
 	@import 'static/css/variables';
+
 	.select-wrapper {
 		position: relative;
 		display: flex;
+		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		padding: .5rem;
+		padding: .75rem;
 		width: 100%;
+
+		&.loading {
+			flex-direction: row;
+		}
 
 		label {
 			cursor: pointer;
-			display: none;
+			width: 100%;
+		}
+
+		.spinner {
+			display: flex;
+			align-items: center;
+			justify-content: center;
 		}
 
 		select {
@@ -46,6 +67,7 @@
 			cursor: pointer;
 			width: 100%;
 			font-size: 1rem;
+			margin-top: .3em;
 
 			&:hover {
 				opacity: 1;
