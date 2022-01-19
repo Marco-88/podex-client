53668d8d2a2ee7d11c4791ef0e976e7e06f462b2
Marco <s66853@beuth-hochschule.de>
2022-01-19 23:17:14 +0100

    Change import of SVGs and add two new icons

--- src/lib/components/icon/icons.ts
+++ src/lib/components/icon/icons.ts
@@ -1,17 +1,21 @@
-import ArrowRight from './svg/arrow-right.svg?component';
-import Check from './svg/check.svg?component';
-import Close from './svg/times.svg?component';
-import Edit from './svg/pen.svg?component';
-import Redo from './svg/redo-alt.svg?component';
-import Spinner from './svg/spinner.svg?component';
-import Toilet from './svg/toilet.svg?component';
-import Trash from './svg/trash-alt.svg?component';
-import Undo from './svg/undo-alt.svg?component';
+import ArrowRight from './svg/arrow-right.svg';
+import Check from './svg/check.svg';
+import Close from './svg/times.svg';
+import Edit from './svg/pen.svg';
+import FullScreen from './svg/expand.svg';
+import Info from './svg/info.svg';
+import Redo from './svg/redo-alt.svg';
+import Spinner from './svg/spinner.svg';
+import Toilet from './svg/toilet.svg';
+import Trash from './svg/trash-alt.svg';
+import Undo from './svg/undo-alt.svg';
 
 export const icons = {
 	check: { top: '.55rem', icon: Check },
 	close: { top: '.4rem', icon: Close },
 	edit: { top: '.45rem', icon: Edit },
+	fullScreen: { icon: FullScreen },
+	info: { icon: Info },
 	redo: { icon: Redo },
 	spinner: { icon: Spinner },
 	submit: { top: '.45rem', icon: ArrowRight },
